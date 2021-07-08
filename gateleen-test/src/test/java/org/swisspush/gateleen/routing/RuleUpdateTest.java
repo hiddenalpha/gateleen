0c43af8ba9c9efdbcf9a0d3dbfff0bb12233a374
Andreas Fankhauser <https://hiddenalpha.ch/slnk/id/1:c0n74ct>
2021-07-08 14:49:42 +0200

    [SDCISA-3180, SDCISA-6067] Travis debugging (#7)
    Just discovered another ugly kludge which now cost me several hours to find out.

--- gateleen-test/src/test/java/org/swisspush/gateleen/routing/RuleUpdateTest.java
+++ gateleen-test/src/test/java/org/swisspush/gateleen/routing/RuleUpdateTest.java
@@ -1,33 +1,7 @@
 package org.swisspush.gateleen.routing;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.parsing.Parser;
-import com.jayway.restassured.response.Response;
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Random;
-
-import static java.nio.charset.StandardCharsets.ISO_8859_1;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 @RunWith(Cucumber.class)
--- gateleen-test/src/test/resources/org/swisspush/gateleen/routing/ProxyALongRunningStream.feature
+++ gateleen-test/src/test/resources/org/swisspush/gateleen/routing/ProxyALongRunningStream.feature
@@ -1,8 +1,12 @@
 
 Feature: Gateleen can proxy long running streams
 
-  Scenario: Long running stream must work properly while rule updates
+  Scenario: Client expects complete response while dawdle downloading a large resource
     Given Mock server is running
     Then Gateleen does not interrupt a running stream
+    Given Mock server is shut down
+
+  Scenario: Client expects correct response while dawdle downloading a large resource
+    Given Mock server is running
     Then Gateleen relays correct content from upstream
     Given Mock server is shut down
